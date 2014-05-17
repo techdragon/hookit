50a96ae8b54c6b4911e4d7e56954d38e21906458
techdragon <lucractius@me.com>
2014-05-17 13:26:53 +0800

    fixing typo in new dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     license='MIT License',
     keywords='git github webhook webhooks',
     description='Bind GitHub WebHooks to actions',
-    install_requires=['docopt >= 0.6.0', 'github3py >= 0.9.0'],
+    install_requires=['docopt >= 0.6.0', 'github3.py >= 0.9.0'],
     author='William Tisäter',
     author_email='william@defunct.cc',
     url='https://github.com/tiwilliam/hookit',
