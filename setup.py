5af6c959eeff8331686d588f69f54144e383d272
techdragon <lucractius@me.com>
2014-06-18 19:35:19 +0800

    sharing the credit properly

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     keywords='git github webhook webhooks',
     description='Bind GitHub WebHooks to actions',
     install_requires=['docopt >= 0.6.0', 'github3.py >= 0.9.0', 'netaddr >= 0.7.11'],
-    author='Samuel Bishop',
-    author_email='sam@psyx.co',
-    url='https://github.com/techdragon/hookit',
+    author='William Tisäter, Samuel Bishop',
+    author_email='william@defunct.cc, sam@psyx.co',
+    url='https://github.com/tiwilliam/hookit',
     entry_points={
         'console_scripts': ['hookit = hookit:run'],
     }
